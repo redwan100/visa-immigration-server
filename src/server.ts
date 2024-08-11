73e6cc5e2c36bee1e9369b13ae0b6feda01d8a03
Redwan islam <ridoneislam987@gmail.com>
2024-08-12 07:43:04 +0800

    change mongodb url

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import config from "./app/config";
 
 async function main() {
   try {
-    // await mongoose.connect(config.database_url as string);
-    await mongoose.connect("mongodb://localhost:27017/visa-immigration");
+    await mongoose.connect(config.database_url as string);
+    // await mongoose.connect("mongodb://localhost:27017/visa-immigration");
     app.listen(config.port, () => {
       console.log(`sunrise app listening on port ${config.port}✅`);
       console.log("server is connected");
